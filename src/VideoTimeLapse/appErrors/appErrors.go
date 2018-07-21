6bb292c3f065171d3dfcaecc78a8fb9ca203233b
sugu <sugeshchandran@gmail.com>
2018-07-21 13:32:24 +0100

    Update the error constants to more generic form to use across the app.
    Signed-off-by: sugu <sugeshchandran@gmail.com>

--- src/VideoTimeLapse/appErrors/appErrors.go
+++ src/VideoTimeLapse/appErrors/appErrors.go
@@ -18,8 +18,8 @@ import (
     "fmt"
 )
 var (
-    INVALID_INPUT = fmt.Errorf("Invalid Input, Cannot process in system")
-    DB_ROW_UNIQUE_ERROR = fmt.Errorf("The Row entry is not unique in the system")
-    DB_ROW_PRESENT_IN_SYSTEM = fmt.Errorf(`The entry already present in System`)
-    DB_ROW_NOT_FOUND = fmt.Errorf("Row entry not found in DB")
+    INVALID_INPUT = fmt.Errorf("Invalid Input, Cannot process in App")
+    DATA_NOT_UNIQUE_ERROR = fmt.Errorf("The entry is not unique in the App")
+    DATA_PRESENT_IN_SYSTEM = fmt.Errorf(`The entry already present in App`)
+    DATA_NOT_FOUND = fmt.Errorf("The entry not found in the Application")
 )
