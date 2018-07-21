8326540800f3a8510b1f02dec74ba8af0fc80232
sugu <sugeshchandran@gmail.com>
2018-07-21 13:18:13 +0100

    Add video timelapse parameters into the camera structure.
    Signed-off-by: sugu <sugeshchandran@gmail.com>

--- src/VideoTimeLapse/dataSet/camera.go
+++ src/VideoTimeLapse/dataSet/camera.go
@@ -18,26 +18,39 @@ const (
     CAMERA_OFF = iota + 1
     CAMERA_ON
     CAMERA_STREAMING
+    CAMERA_DELETED
 )
-
 //Can be one of the values in the above.
 type CameraStatus uint64
 
+//Type of camera protocol.
+const (
+    CAMERA_TYPE_RTSP = iota + 1
+)
+
+//Default value for some of camera parameters.
+const (
+    //default recording time for a camera.
+    CAMERA_DEFAULT_TIMELAPSE_SEC = 3600 //(1 Hr)
+)
 //Structure to hold all the information for the camera.
-//Must update JsonCamera when updating this structure.
+//Must update JsonCameraInput when updating this structure.
 type Camera struct {
     Name   string        `json:"Name"`
     Ipaddr string        `json:"Ipaddr"`
     Port   string        `json:"Port"`
     Desc   string        `json:"Desc"`
     Status CameraStatus  `json:"Status"`
+    Type   uint64        `json:"Type"`
     UserId string        `json:"UserId"`
     Pwd string           `json:"Pwd"`
+    //Total video recording time for timelapse video.
+    VideoLenSec uint64   `json:"VideoLenSec"`
 }
 
 func (camObj *Camera) IsCameraStatusValid() (bool, error) {
     if camObj.Status < CAMERA_OFF ||
-        camObj.Status > CAMERA_STREAMING {
+        camObj.Status > CAMERA_DELETED {
         return false, nil
     }
     return true, nil
